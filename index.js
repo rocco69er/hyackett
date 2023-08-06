2ed4a2da1db5cd4e730e1d8a1c3dd26ef532e07c
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2023-08-06 06:24:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
+const parseTorrent = require("parse-torrent");
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
-const parseTorrent = require("parse-torrent");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const type_ = {
   MOVIE: "movie",
@@ -14,7 +16,7 @@ const toStream = (parsed, tor, type, s, e) => {
   let title = tor.extraTag || parsed.name;
   let index = -1;
   if (type === "series") {
-    index = (parsed.files ?? []).findIndex((element, index) => {
+    index = (parsed.files ?? []).findIndex((element) => {
       return (
         element["name"]?.toLowerCase()?.includes(`s0${s}`) &&
         element["name"]?.toLowerCase()?.includes(`e0${e}`) &&
@@ -36,11 +38,9 @@ const toStream = (parsed, tor, type, s, e) => {
     type: type,
     infoHash: infoHash,
     fileIdx: index == -1 ? 1 : index,
-    sources: (parsed.announce || [])
-      .map((x) => {
-        return "tracker:" + x;
-      })
-      .concat(["dht:" + infoHash]),
+    sources: (parsed.announce || []).map((x) => {
+      return "tracker:" + x;
+    }).concat(["dht:" + infoHash]),
     title: title,
   };
 };
@@ -63,22 +63,60 @@ const streamFromMagnet = (tor, uri, type, s, e) => {
 let stream_results = [];
 let torrent_results = [];
 
-let host = "http://100.40.225.234:9117"; // Update to your desired host
-let apiKey = "fql6lmpnr2xnw2om8s7arfcmkpd2jinw"; // Update to your desired API key
+let host = "http://100.40.225.234:9117";
+let apiKey = "fql6lmpnr2xnw2om8s7arfcmkpd2jinw";
 
 let fetchTorrent = async (query) => {
-  try {
-    let url = `${host}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&_=1691241987837`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Error fetching data from the API");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return [];
-  }
+  let url = `${host}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&_=1691241987837`;
+  return await fetch(url, {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      "x-requested-with": "XMLHttpRequest",
+    },
+    referrerPolicy: "no-referrer",
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then(async (results) => {
+      if (results["Results"].length != 0) {
+        torrent_results = await Promise.all(
+          results["Results"].map((result) => {
+            return new Promise((resolve, reject) => {
+              resolve({
+                Tracker: result["Tracker"],
+                Category: result["CategoryDesc"],
+                Title: result["Title"],
+                Seeders: result["Seeders"],
+                Peers: result["Peers"],
+                Link: result["Link"],
+              });
+            });
+          })
+        );
+        return torrent_results;
+      } else {
+        return [];
+      }
+    });
 };
+
+function getMeta(id, type) {
+  var [tt, s, e] = id.split(":");
+  return fetch(`https://v2.sg.media-imdb.com/suggestion/t/${tt}.json`)
+    .then((res) => res.json())
+    .then((json) => json.d[0])
+    .then(({ l, y }) => ({ name: l, year: y }))
+    .catch((err) =>
+      fetch(`https://v3-cinemeta.strem.io/meta/${type}/${tt}.json`)
+        .then((res) => res.json())
+        .then((json) => json.meta)
+    );
+}
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/manifest.json", (req, res) => {
   var json = {
@@ -86,10 +124,12 @@ app.get("/manifest.json", (req, res) => {
     version: "3.0.0",
     name: "Hackett",
     description: "Torrent results from Jackett Indexers",
-    icon: "https://raw.githubusercontent.com/mikmc55/stremio-jackett/main/hy.jpg",
+    icon:
+      "https://raw.githubusercontent.com/mikmc55/stremio-jackett/main/hy.jpg",
     resources: ["stream"],
     types: ["movie", "series"],
     idPrefixes: ["tt"],
+    catalogs: [],
   };
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
@@ -98,30 +138,39 @@ app.get("/manifest.json", (req, res) => {
 });
 
 app.get("/stream/:type/:id", async (req, res) => {
-  try {
-    const media = req.params.type === "series" ? "TV" : "MOVIE";
-    const [tt, s, e] = req.params.id.split(":");
-    const query = encodeURIComponent(tt);
-    const result = await fetchTorrent(query);
+  const media = req.params.type;
+  let id = req.params.id;
+  id = id.replace(".json", "");
 
-    let stream_results = await Promise.all(
-      result.map((torrent) => {
-        return streamFromMagnet(torrent, torrent["Link"], media, s, e);
-      })
-    );
+  let [tt, s, e] = id.split(":");
+  let query = "";
+  let meta = await getMeta(tt, media);
 
-    // Send response
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Content-Type", "application/json");
-    return res.send({ streams: stream_results });
-  } catch (error) {
-    console.error("Error processing request:", error);
-    // Send error response
-    res.status(500).json({ error: "Internal Server Error" });
+  query = meta?.name;
+
+  if (media === "movie") {
+    query += " " + meta?.year;
+  } else if (media === "series") {
+    query += " S" + (s ?? "1").padStart(2, "0");
   }
+
+  query = encodeURIComponent(query);
+
+  let result = await fetchTorrent(query);
+
+  let stream_results = await Promise.all(
+    result.map((torrent) => {
+      return streamFromMagnet(torrent, torrent["Link"], media, s, e);
+    })
+  );
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Content-Type", "application/json");
+
+  return res.send({ streams: stream_results });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("The server is working on " + process.env.PORT || 3000);
+  console.log("The server is working on " + (process.env.PORT || 3000));
 });
