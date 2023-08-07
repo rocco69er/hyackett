94abf1601d5e434b7771fc5c27a48c1def81a451
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2023-08-07 21:37:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ app.get("/manifest.json", (req, res) => {
     version: "3.0.0",
     name: "HYJackett",
     description: "Movie & TV Streams from Jackett",
-    logo: "https://raw.githubusercontent.com/daniwalter001/daniwalter001/main/52852137.png",
+    logo: "https://raw.githubusercontent.com/mikmc55/hyackett/main/hyjackett.jpg",
     resources: ["stream"],
     types: ["movie", "series"],
     idPrefixes: ["tt"],
